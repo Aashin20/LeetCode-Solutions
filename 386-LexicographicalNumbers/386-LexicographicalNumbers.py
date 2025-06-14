47893bed5ef0034a7d65a18edea6f03b09d430c6
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-14 22:33:21 +0530

    Create 386-LexicographicalNumbers

---
+++ 386-LexicographicalNumbers/386-LexicographicalNumbers.py
@@ -0,0 +1,18 @@
+# Last updated: 6/14/2025, 10:33:14 PM
+class Solution(object):
+    def lexicalOrder(self, n):
+        result = []
+        current = 1
+
+        for _ in range(n):
+            result.append(current)
+            if current * 10 <= n:
+                current *= 10  
+            else:
+                if current >= n:
+                    current //= 10 
+                current += 1
+                while current % 10 == 0:
+                    current //= 10 
+
+        return result
