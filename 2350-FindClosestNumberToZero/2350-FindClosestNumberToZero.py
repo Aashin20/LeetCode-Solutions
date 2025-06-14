809f41cab3815843f0e088c1fcceb9646a862f69
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-14 22:32:28 +0530

    Create 2350-FindClosestNumberToZero

---
+++ 2350-FindClosestNumberToZero/2350-FindClosestNumberToZero.py
@@ -0,0 +1,19 @@
+# Last updated: 6/14/2025, 10:32:21 PM
+class Solution:
+    def findClosestNumber(self, nums):
+        len_nums = len(nums)
+        
+        min_dist_to_zero = None
+        closest_to_zero_num = None
+
+        i = 0
+        while (i < len_nums):
+            num = nums[i]
+
+            if (min_dist_to_zero is None or abs(num-0) <= min_dist_to_zero):
+                closest_to_zero_num = max(num, closest_to_zero_num) if min_dist_to_zero == abs(num-0) else num
+                min_dist_to_zero = abs(num-0)
+
+            i += 1
+
+        return closest_to_zero_num
