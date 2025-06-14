ab1f21b1e69231df9c150bfb0e29fadf157f26ef
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-14 22:32:11 +0530

    Create 2720-MinimizeTheMaximumDifferenceOfPairs

---
+++ 2720-MinimizeTheMaximumDifferenceOfPairs/2720-MinimizeTheMaximumDifferenceOfPairs.py
@@ -0,0 +1,26 @@
+# Last updated: 6/14/2025, 10:32:04 PM
+class Solution(object):
+    def minimizeMax(self, nums, p):
+        nums.sort()
+        n = len(nums)
+
+        def can_form_pairs(max_diff):
+            count = 0
+            i = 1
+            while i < n:
+                if nums[i] - nums[i - 1] <= max_diff:
+                    count += 1
+                    i += 2 
+                else:
+                    i += 1
+            return count >= p
+
+        left, right = 0, int(1e9)
+        while left < right:
+            mid = (left + right) // 2
+            if can_form_pairs(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
