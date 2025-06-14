fe269b3e243667ba1cc2b4ee94891dac492acaae
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-14 22:33:54 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.cpp
@@ -0,0 +1,13 @@
+// Last updated: 6/14/2025, 10:33:48 PM
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            ans=ans^nums[i];
+           
+
+        } 
+        return ans;
+    }
+};
