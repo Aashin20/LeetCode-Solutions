e625fd01f9dda6621898a43e9d68d3dcb434e53f
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-14 22:32:18 +0530

    Create 2551-ApplyOperationsToAnArray

---
+++ 2551-ApplyOperationsToAnArray/2551-ApplyOperationsToAnArray.py
@@ -0,0 +1,17 @@
+# Last updated: 6/14/2025, 10:32:12 PM
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        for i in range(n-1):
+            if nums[i] == nums[i+1] and nums[i] != 0: 
+                nums[i] *= 2
+                nums[i+1] = 0
+        result = []
+        zeros = []
+        for num in nums:
+            if num != 0:
+                result.append(num)
+            else:
+                zeros.append(0)
+                
+        return result + zeros
