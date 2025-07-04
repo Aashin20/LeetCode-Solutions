6bb1992da4605cea3cded6dfee9f575302cc4ec6
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-07-04 18:13:46 +0530

    Create 3307-FindtheK-thCharacterinStringGameII

---
+++ 3307-FindtheK-thCharacterinStringGameII/version/python/3307-FindtheK-thCharacterinStringGameII_20250704_181346.py
@@ -0,0 +1,15 @@
+# Last updated: 7/4/2025, 6:13:46 PM
+class Solution:
+    def kthCharacter(self, k: int, operations: List[int]) -> str:
+        n, i = 1, 0
+        while n < k:
+            n *= 2
+            i += 1
+        d = 0
+        while n > 1:
+            if k > n // 2:
+                k -= n // 2
+                d += operations[i - 1]
+            n //= 2
+            i -= 1
+        return chr(d % 26 + ord("a"))
