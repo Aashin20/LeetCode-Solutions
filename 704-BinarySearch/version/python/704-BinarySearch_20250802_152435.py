433d4d5b3925f316c81b62f7fbace377331eadd6
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-08-02 15:24:35 +0530

    Create 704-BinarySearch

---
+++ 704-BinarySearch/version/python/704-BinarySearch_20250802_152435.py
@@ -0,0 +1,11 @@
+# Last updated: 8/2/2025, 3:24:35 PM
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res=[[1]]
+        for i in range(numRows-1):
+            temp=[0]+res[-1]+[0]
+            row=[]
+            for j in range(len(res[-1])+1):
+                row.append(temp[j]+temp[j+1])
+            res.append(row)
+        return res
