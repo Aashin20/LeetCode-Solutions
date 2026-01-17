9ab822e5054345e9073ec51d21c82f54f77eefb3
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2026-01-17 23:16:03 +0530

    Create 881-BoatstoSavePeople

---
+++ 881-BoatstoSavePeople/version/python/881-BoatstoSavePeople_20260117_231602.py
@@ -0,0 +1,12 @@
+# Last updated: 1/17/2026, 11:16:02 PM
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        l, r = 0, len(people) - 1
+        boats = 0
+        while l <= r:
+            if people[l] + people[r] <= limit:
+                l += 1
+            r -= 1
+            boats += 1
+        return boats
