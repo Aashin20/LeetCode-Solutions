574b8437c09a7aa9f0d069125407ce8b2338f03a
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2026-04-01 23:30:08 +0530

    Create 1631-PathWithMinimumEffort

---
+++ 1631-PathWithMinimumEffort/version/python/1631-PathWithMinimumEffort_20260401_233007.py
@@ -0,0 +1,24 @@
+# Last updated: 4/1/2026, 11:30:07 PM
+1import heapq
+2class Solution:
+3    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+4        ROWS,COL = len(heights),len(heights[0])
+5        min_heap = [[0,0,0]]
+6        visit = set()
+7        directions = [[0,1],[0,-1],[1,0],[-1,0]]
+8        while min_heap:
+9            diff,r,c = heapq.heappop(min_heap)
+10            if (r,c) in visit:
+11                continue
+12            visit.add((r,c))
+13            if (r,c) == (ROWS-1,COL-1):
+14                return diff
+15            for dr,dc in directions:
+16                newR,newC = r+dr,c+dc
+17                if (newR==ROWS or newC==COL or newR < 0 or newC<0 or (newR,newC) in visit):
+18                    continue
+19                newDiff = max(diff,abs(heights[r][c]-heights[newR][newC]))
+20                heapq.heappush(min_heap,[newDiff,newR,newC])
+21            
+22            
+23
