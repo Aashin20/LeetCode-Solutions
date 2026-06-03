ac9952cc5ad433539358d94677df66703618e9c1
Aashin C Anil <aa2387@srmist.edu.in>
2026-06-03 11:03:02 +0530

    Create 1046-LastStoneWeight

---
+++ 1046-LastStoneWeight/version/python/1046-LastStoneWeight_20260603_110259.py
@@ -0,0 +1,15 @@
+# Last updated: 6/3/2026, 11:02:59 AM
+1import heapq
+2class Solution:
+3    def lastStoneWeight(self, stones: List[int]) -> int:
+4        heap=[-i for i in stones]
+5        heapq.heapify(heap)
+6        while heap:
+7            if len(heap)==1:
+8                return -heap[0]
+9            x=-heapq.heappop(heap)
+10            y=-heapq.heappop(heap)
+11
+12            if x!=y:
+13                heapq.heappush(heap,-(x-y))
+14        return 0
