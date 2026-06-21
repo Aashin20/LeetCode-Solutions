903215973d3e43e345588bd13e8983b339b2a518
Aashin C Anil <aa2387@srmist.edu.in>
2026-06-21 15:16:22 +0530

    Create 977-SquaresofaSortedArray

---
+++ 977-SquaresofaSortedArray/version/python/977-SquaresofaSortedArray_20260621_151622.py
@@ -0,0 +1,17 @@
+# Last updated: 6/21/2026, 3:16:22 PM
+1class Solution:
+2    def sortedSquares(self, nums: List[int]) -> List[int]:
+3        L=0
+4        R=len(nums)-1
+5        n=len(nums)
+6        idx=n-1
+7        res=[0]*n
+8        while L<=R:
+9            if abs(nums[L])<abs(nums[R]):
+10                res[idx]=nums[R]*nums[R]
+11                R-=1
+12            else:
+13                res[idx]=nums[L]*nums[L]
+14                L+=1
+15            idx-=1
+16        return res
