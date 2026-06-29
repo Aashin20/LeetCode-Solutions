be9706cc9ce5712c422d675d994d9be1168af177
Aashin C Anil <aa2387@srmist.edu.in>
2026-06-29 22:39:19 +0530

    Create 42-TrappingRainWater

---
+++ 42-TrappingRainWater/version/python/42-TrappingRainWater_20260629_223918.py
@@ -0,0 +1,20 @@
+# Last updated: 6/29/2026, 10:39:18 PM
+1class Solution:
+2    def trap(self, height: List[int]) -> int:
+3        maxL=0
+4        maxR=0
+5        hL=[maxL]*len(height)
+6        hR=[maxR]*len(height)
+7        n=len(height)
+8        for i in range(n):
+9            j=-i-1
+10            hL[i]=maxL
+11            hR[j]=maxR
+12            maxL=max(maxL,height[i])
+13            maxR=max(maxR,height[j])
+14        
+15        summ=0
+16        for i in range(n):
+17            pot=min(hL[i],hR[i])
+18            summ+=max(0,pot-height[i])
+19        return summ
