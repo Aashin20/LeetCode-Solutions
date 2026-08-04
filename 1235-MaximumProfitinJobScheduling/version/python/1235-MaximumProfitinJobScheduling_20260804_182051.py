b823038b419377bcea46a58f98f268531a55a5c2
Aashin C Anil <aa2387@srmist.edu.in>
2026-08-04 18:20:53 +0530

    Create 1235-MaximumProfitinJobScheduling

---
+++ 1235-MaximumProfitinJobScheduling/version/python/1235-MaximumProfitinJobScheduling_20260804_182051.py
@@ -0,0 +1,29 @@
+# Last updated: 8/4/2026, 6:20:51 PM
+1class Solution:
+2    def maxValue(self, events: List[List[int]], k: int) -> int:
+3        events.sort()
+4        memo={}
+5        res=0
+6        def dfs(i,k):
+7            if i==len(events): return 0
+8            if k==0: return 0
+9            if (i,k) in memo: return memo[(i,k)]
+10            res=dfs(i+1,k)
+11            j=bisect.bisect_left(events,[events[i][1]+1,-1,-1])
+12            res=max(res,events[i][2]+dfs(j,k-1))
+13            memo[(i,k)]=res
+14            return res
+15        
+16        # def binary_search(L,target):
+17        #     R=len(events)-1
+18        #     ans=len(events)
+19        #     while L<=R:
+20        #         M=(L+R)//2
+21        #         if events[M][0]>target:
+22        #             ans=M
+23        #             R=M-1
+24        #         else:
+25        #             L=M+1
+26        #     return ans
+27        # return dfs(0,k)
+28        return dfs(0,k)
